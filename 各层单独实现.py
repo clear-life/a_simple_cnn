24766d2252dca1fdd2ce53939c1982b1e540bd90
clearlife <1336509100@qq.com>
2021-08-18 20:52:31 +0800

    注释

--- 各层单独实现.py
+++ 各层单独实现.py
@@ -99,25 +99,26 @@ class Affine:
 
 # 卷积层
 class Convolution:
-    # W 是四维数据, b 是三维数据
+    # W 是四维数据, b 是一数据
     def __init__(self, W, b, stride = 1, pad = 0):  # 步幅默认为 1, 填充默认为 0
-        self.W = W
-        self.b = b
-        self.stride = stride
-        self.pad = pad
+        # 卷积层的参数
+        self.W = W              # 权重参数
+        self.b = b              # 偏置参数
+        self.stride = stride    # 步幅
+        self.pad = pad          # 填充
 
         # 反向传播时计算梯度用到的数据
-        self.x = None
-        self.col_x = None
-        self.col_W = None
+        self.x = None           # 该层的输入数据
+        self.col_x = None       # 输入数据的二维格式
+        self.col_W = None       # 权重参数的二维格式
 
-        # 梯度
-        self.dW = None
-        self.db = None
+        # 记录梯度
+        self.dW = None          # 损失函数 L 对权重 W 的梯度
+        self.db = None          # 损失函数 L 对偏置 b 的梯度
 
     # x 是四维数据
     def forward(self, x):
-        # 记录输入数据和权重 shape 的相应参数
+        # 记录输入数据和权重的 shape
         N, C, H, W = x.shape
         FN, C, FH, FW = self.W.shape
 
@@ -125,14 +126,14 @@ class Convolution:
         OH = int(1 + (H + 2 * self.pad - FH) / self.stride)
         OW = int(1 + (W + 2 * self.pad - FW) / self.stride)
 
-        # x 转化为二维数据, 方便进行矩阵乘法运算
+        # x 和 W 转化为二维数据, 方便进行矩阵乘法运算
         col_x = im2col(x, FH, FW, self.stride, self.pad)
         col_W = self.W.reshape(FN, -1).T    # -1 表示自动计算, T 表示竖向展开
 
         # 计算结果
         output = np.dot(col_x, col_W) + self.b
 
-        # 重塑结果 shape
+        # 重塑结果的 shape
         output = output.reshape(N, OH, OW, -1).transpose(0,3,1,2)
 
         # 记录数据
@@ -143,13 +144,16 @@ class Convolution:
         return output
 
     def backward(self, doutput):
+        # 取出 W 的 shape, 重塑 doutput 的shape
         FN, C, FH, FW = self.W.shape
         doutput = doutput.transpose(0, 2, 3, 1).reshape(-1, FN)
 
+        # 根据公式计算并记录相应的梯度
         self.db = np.sum(doutput, axis = 0)
         self.dW = np.dot(self.col_x.T, doutput)
         self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
 
+        # 计算 dx
         dcol_x = np.dot(doutput, self.col_W.T)
         dx = col2im(dcol_x, self.x.shape, FH, FW, self.stride, self.pad)
 
@@ -158,8 +162,9 @@ class Convolution:
 
 # 池化层
 class Pooling:
-    # 池化层的目标区域高和宽
+    # pool_h 和 pool_w 表示池化层的目标区域高和宽
     def __init__(self, pool_h, pool_w, stride=1, pad = 0):
+        # 记录参数
         self.pool_h = pool_h
         self.pool_w = pool_w
         self.stride = stride
